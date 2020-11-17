5ca42d1dfae5845ce298d2d690870e3c1f862a72
levant47 <levant.ba@gmail.com>
2020-11-17 08:15:11 +0200

    Done

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace SPZLab4Var1
+{
+    class Program
+    {
+        private const string _inputFileName = "text.txt";
+
+        static void Main() => File.WriteAllText(
+            _inputFileName + ".out",
+            File.ReadAllText(_inputFileName)
+                .Split(", ")
+                .GroupBy(character => character)
+                .Max(grouping => grouping.Count())
+                .ToString()
+        );
+    }
+}
+
